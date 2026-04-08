5e19aba932cd2484fb9f5bf314748de798cc9d55
Rafael Stedile <rafaelstedile@gmail.com>
2026-04-08 00:31:32 -0300

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,10 +2,10 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   locales: ['en', 'pt', 'es', 'fr', 'de', 'ar'],
-  defaultLocale: 'pt'
+  defaultLocale: 'pt',
+  localePrefix: 'always'
 });
 
 export const config = {
-  // Matcher padrão que a Vercel entende perfeitamente
-  matcher: ['/', '/(de|en|es|fr|pt|ar)/:path*']
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
