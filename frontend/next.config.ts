254e38f32301bdae94285d85ecdb0c4b004215ac
Rafael Stedile <rafaelstedile@gmail.com>
2026-04-08 09:56:21 -0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,16 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts')
 
 const nextConfig: NextConfig = {
-  // Isso ajuda a evitar erros de rota no servidor
-  trailingSlash: true,
+  // O Next.js vai interceptar quem acessar a raiz e jogar direto para a página pronta
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pt/compare/brazil-2026',
+        permanent: false, 
+      },
+    ]
+  },
 }
 
 export default withNextIntl(nextConfig)
